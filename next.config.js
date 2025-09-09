24f497177d53699b135aedf87992e18d04d5ca76
outrelinkza <outrelink@gmail.com>
2025-09-09 22:09:44 +0200

    Disable all caching to force browser refresh

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  generateEtags: false,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
